ff43acdd8db4e441857a412c12f65a580726c4eb
Mark Pena <markdrew53@gmail.com>
2018-07-22 23:20:17 -0500

    modify the message

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const { createServer } =  require('http')
 const port = process.env.PORT || 3000
 
 function webServer(req, res) {
-    res.end('<h1> Now + GitHub</h1>')
+    res.end(`
+        <h1>Now + GitHub </h1>
+
+        <header>
+            <img src="https://cdn-images-1.medium.com/max/1600/1*HwLQ0aoCqrkrRNFuIEQ_dQ.png" alt="ZEIT Ico" height="360" width="480">
+        </header>
+    `)
 }
 
 const server = createServer(webServer)
